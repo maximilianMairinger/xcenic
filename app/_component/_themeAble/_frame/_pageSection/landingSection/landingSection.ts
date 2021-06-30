be5c3876365f3d4863bacccc2695776ee2e2ac47
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-06-30 12:59:49 +0200

    view work with link animations

--- app/_component/_themeAble/_frame/_pageSection/landingSection/landingSection.ts
+++ app/_component/_themeAble/_frame/_pageSection/landingSection/landingSection.ts
@@ -7,21 +7,28 @@ import RippleButton from "./../../../_button/_rippleButton/rippleButton"
 import "./../../../_button/button"
 import Button from "./../../../_button/button"
 import "./../../../link/link"
+import Link from "./../../../link/link"
 import "./../../../_icon/bigVideo/bigVideo"
 import "./../../../_icon/landingCircle/landingCircle"
 import "./../../../textBlob/textBlob"
+import { EventListener } from "extended-dom"
 
 export default class LandingSection extends PageSection {
 
   private coverButton = this.q("view-work .coverButton") as Button
   private rippleButton = this.q("view-work .rippleButton") as RippleButton
+  private link = this.q("view-work c-link") as Link
   constructor() {
     super("light")
 
+    this.link.tabIndex = undefined
+    new EventListener(this.coverButton, ["mouseover", "focus"], this.link.mouseOverAnimation)
+    new EventListener(this.coverButton, ["mouseleave", "blur"], this.link.mouseOutAnimation)
+    // this.coverButton.addActivationCallback(this.link.clickAnimation)
+
     this.coverButton.on("mousedown", () => {
       let release = this.rippleButton.initRipple();
-      this.coverButton.on("mouseup", release)
-      this.coverButton.on("mouseout", release)
+      new EventListener(this.coverButton, ["mouseup", "mouseout"], release, undefined, {once: true})
     })
   }
 
