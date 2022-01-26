f610e84f1e80560281dca5f22c484bde0b4ac0a7
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2022-01-26 21:59:44 +0100

    wip self made pinchAndPan

--- app/_component/_themeAble/_frame/_page/adminPage/adminPage.ts
+++ app/_component/_themeAble/_frame/_page/adminPage/adminPage.ts
@@ -3,11 +3,49 @@ import Page from "../page"
 
 import adminSession from "../../../../../lib/adminSession"
 
+import ContactPage from "./../contactPage/contactPage"
+import { ElementList } from "extended-dom"
+
+
 
 export default class AdminPage extends Page {
 
   constructor() {
     super()
+
+    this.body.canvas.apd(
+      ce("page-frame").apd(
+        ce("test-box")
+      )
+    )
+
+
+
+
+
+
+    const panArea = this.body.panArea
+    const pinchArea = this.body.pinchArea
+
+    this.body.canvas.on("mousemove", (e) => {
+      pinchArea.css("transformOrigin", `${e.clientX}px ${e.clientY}px` as any)
+    })
+
+
+    this.body.canvas.on("wheel", (e: WheelEvent) => {
+      e.preventDefault();
+      if (e.ctrlKey) {
+        pinchArea.css({
+          scale: pinchArea.css("scale") * (e.deltaY > 0 ? 0.99 : 1.01)
+        })
+        
+      } else {
+        panArea.css({
+          translateX: panArea.css("translateX") - e.deltaX, 
+          translateY: panArea.css("translateY") - e.deltaY
+        })
+      }
+    }, {passive: false})
 
 
   }
@@ -15,6 +53,23 @@ export default class AdminPage extends Page {
   protected tryNavigationCallback(domainFragment: string): boolean | void | Promise<boolean | void> {
     return adminSession.get() !== ""
   }
+  
+  // private pageLs: ElementList<Page>
+  // public async minimalContentPaint(): Promise<void> {
+  //   this.pageLs = this.body.canvas.childs("page-frame > *", true) as ElementList<Page>
+  //   await Promise.all([this.pageLs.map((page: Page) => page.minimalContentPaint())])
+  //   await super.minimalContentPaint()
+  // }
+
+  // public async fullContentPaint(): Promise<void> {
+  //   await Promise.all([this.pageLs.map((page: Page) => page.fullContentPaint())])
+  //   await super.fullContentPaint()
+  // }
+
+  // public async completePaint(): Promise<void> {
+  //   await Promise.all([this.pageLs.map((page: Page) => page.completePaint())])
+  //   await super.completePaint()
+  // }
 
 
 
