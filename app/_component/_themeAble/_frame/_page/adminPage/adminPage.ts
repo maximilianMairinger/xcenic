6bc8dee19f1eaf2772544440e8b74008af22e3f8
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2022-01-27 17:53:48 +0100

    admin page overflow bounce working cleanly now

--- app/_component/_themeAble/_frame/_page/adminPage/adminPage.ts
+++ app/_component/_themeAble/_frame/_page/adminPage/adminPage.ts
@@ -18,6 +18,85 @@ export default class AdminPage extends Page {
 
     this.body.canvas.apd(
       ce("page-frame").apd(
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
+        ce("test-box"),
         ce("test-box")
       )
     )
@@ -25,25 +104,6 @@ export default class AdminPage extends Page {
 
     
 
-    const contrIsPanInDirIdle = () => {
-      const isPanIdle = new Data(false)
-      let panIdleTimer = setTimeout(() => {})
-      const resetPanIdleTimer = () => {
-        console.log("resetTImer")
-        isPanIdle.set(false)
-        clearTimeout(panIdleTimer)
-        panIdleTimer = setTimeout(() => {
-          panIdleTimer = setTimeout(() => {
-            isPanIdle.set(true)
-          }, 100)
-        }, 50)
-      }
-
-      return {resetPanIdleTimer, isPanIdle}
-    }
-
-    const { isPanIdle: isPanXIdle, resetPanIdleTimer: resetPanXIdleTimer } = contrIsPanInDirIdle()
-    const { isPanIdle: isPanYIdle, resetPanIdleTimer: resetPanYIdleTimer } = contrIsPanInDirIdle()
 
 
     const target = this.body.panArea
@@ -61,208 +121,40 @@ export default class AdminPage extends Page {
       y: 0
     }
 
-
-    const lastAbs = {
+    const delta = {
       x: 0,
       y: 0
     }
-
-    const lastLastAbs = {
-      x: 0,
-      y: 0
-    }
-
-    const lastHowFarOutOfBounds = {
-      x: 0,
-      y: 0
-    }
-
-
     this.body.canvasContainer.on("wheel", (e: WheelEvent) => {
       e.preventDefault();
       if (!e.ctrlKey) {
+        delta.x = e.deltaX
+        delta.y = e.deltaY
 
-        lastLastAbs.x = lastAbs.x
-        lastLastAbs.y = lastAbs.y
-
-        lastAbs.x = abs.x
-        lastAbs.y = abs.y
-
-        
-
-        abs.x -= e.deltaX
-        abs.y -= e.deltaY
+        abs.x -= delta.x
+        abs.y -= delta.y
 
 
-       
-        // slowly make it harder to move when out of bounds
-        if (howFarOutOfBounds.x < 0) {
-          const impact = (howFarOutOfBounds.x / 70) * -1
-          if (e.deltaX < 0) abs.x += impact * e.deltaX
-          abs.y += impact * e.deltaY * .75
-        }
-        else if (howFarOutOfBounds.x > 0) {
-          const impact = (howFarOutOfBounds.x / 70)
-          if (e.deltaX > 0) abs.x += impact * e.deltaX
-          abs.y += impact * e.deltaY * .75
-        }
-        if (howFarOutOfBounds.y < 0) {
-          const impact = (howFarOutOfBounds.y / 70) * -1
-          if (e.deltaY < 0) abs.y += impact * e.deltaY
-          abs.x += impact * e.deltaX * .75
-        }
-        else if (howFarOutOfBounds.y > 0) {
-          const impact = (howFarOutOfBounds.y / 70)
-          if (e.deltaY > 0) abs.y += impact * e.deltaY
-          abs.x += impact * e.deltaX * .75
-        }
-
-        
-
-        lastHowFarOutOfBounds.x = howFarOutOfBounds.x
-        lastHowFarOutOfBounds.y = howFarOutOfBounds.y
-
-        howFarOutOfBounds.x = abs.x > 0 ? -abs.x : abs.x < target.width() - child.width() ? target.width() - child.width() - abs.x : -0
-        howFarOutOfBounds.y = abs.y > 0 ? -abs.y : abs.y < target.height() - child.height() ? target.height() - child.height() - abs.y : 0
-        
-
-
-
-        
-
-        e.stopPropagation()
-
-        if (lastAbs.x - abs.x > .4) {
-          if (isPanXIdle.get()) {
-            if (lastBoyX !== false && xWasAtIdelOutOfBounds) {
-              if (howFarOutOfBounds.x >= 0) abs.x = lastAbs.x
-              else resetPanXIdleTimer()
-            }
-            else {
-              if (howFarOutOfBounds.x > 0) abs.x = lastAbs.x
-              else resetPanXIdleTimer()
-            }
-        
-            
-          }
-          else resetPanXIdleTimer()
-        }
-        else if (lastAbs.x - abs.x < -.4) {
-          if (isPanXIdle.get()) {
-        
-            if (lastBoyX !== true && xWasAtIdelOutOfBounds) {
-              if (howFarOutOfBounds.x <= 0) abs.x = lastAbs.x
-              else resetPanXIdleTimer()
-            }
-            else {
-              if (howFarOutOfBounds.x < 0) abs.x = lastAbs.x
-              else resetPanXIdleTimer()
-              
-            }
-          }
-          else resetPanXIdleTimer()
-        }
-
-        if (lastAbs.y - abs.y > .4) {
-          if (isPanYIdle.get()) {
-            if (lastBoyY !== false && yWasAtIdelOutOfBounds) {
-              if (howFarOutOfBounds.y >= 0) abs.y = lastAbs.y
-              else resetPanYIdleTimer()
-            }
-            else {
-              if (howFarOutOfBounds.y > 0) abs.y = lastAbs.y
-              else resetPanYIdleTimer()
-            }
-          }
-          else resetPanYIdleTimer()
-        }
-        else if (lastAbs.y - abs.y < -.4) {
-          if (isPanYIdle.get()) {
-            if (lastBoyY !== true && yWasAtIdelOutOfBounds) {
-              if (howFarOutOfBounds.y <= 0) abs.y = lastAbs.y
-              else resetPanYIdleTimer()
-            }
-            else {
-              if (howFarOutOfBounds.y < 0) abs.y = lastAbs.y
-              else resetPanYIdleTimer()
-            }
-          }
-          else resetPanYIdleTimer()
-        }
-
-
-        target.css({
-          translateX: abs.x,
-          translateY: abs.y
-        })
-
-        
+        e.stopPropagation()  
       }
+
     }, true)
 
 
-    let lastBoyX: boolean = undefined
-    let lastBoyY: boolean = undefined
+
+    animationFrameDelta(() => {
+      howFarOutOfBounds.x = abs.x > 0 ? -abs.x : abs.x < target.width() - child.width() ? target.width() - child.width() - abs.x : 0
+      howFarOutOfBounds.y = abs.y > 0 ? -abs.y : abs.y < target.height() - child.height() ? target.height() - child.height() - abs.y : 0
+
+      if (howFarOutOfBounds.x !== 0) abs.x += howFarOutOfBounds.x * .4
+      if (howFarOutOfBounds.y !== 0) abs.y += howFarOutOfBounds.y * .4
 
 
-    
-
-    
-
-
-    const constrFadeBackIntoViewPortAnimationForDir = (dir: "x" | "y") => {
-      const translateKey = dir === "x" ? "translateX" : "translateY"
-      const cssOb = {}
-      cssOb[translateKey] = 0
-
-      const fadeBackIntoViewPortAnimation = animationFrameDelta((delta) => {
-        // slowly fade viewport into bounds
-        if (howFarOutOfBounds[dir] < 0) abs[dir] -= Math.max(-delta * howFarOutOfBounds[dir] / 7, .4)
-        else if (howFarOutOfBounds[dir] > 0) abs[dir] += Math.max(delta * howFarOutOfBounds[dir] / 7, .4)
-
-        howFarOutOfBounds[dir] = abs[dir] > 0 ? -abs[dir] : abs[dir] < target.width() - child.width() ? target.width() - child.width() - abs[dir] : 0
-  
-        cssOb[translateKey] = abs[dir]
-        target.css(cssOb)
-  
-        
+      target.css({
+        translateX: abs.x,
+        translateY: abs.y
       })
-
-      fadeBackIntoViewPortAnimation.cancel()
-
-      return fadeBackIntoViewPortAnimation
-    }
-
-
-
-    const xFadeBackIntoView = constrFadeBackIntoViewPortAnimationForDir("x")
-    const yFadeBackIntoView = constrFadeBackIntoViewPortAnimationForDir("y")
-
-
-    let xWasAtIdelOutOfBounds = false
-    isPanXIdle.get((idle) => {
-      if (idle) {
-        console.log("idle x")
-        lastBoyX = howFarOutOfBounds.x > 0 ? true : howFarOutOfBounds.x < 0 ? false : undefined
-        xWasAtIdelOutOfBounds = howFarOutOfBounds.x !== 0
-        xFadeBackIntoView.resume()
-      }
-      else xFadeBackIntoView.cancel()
-    }, false)
-
-    let yWasAtIdelOutOfBounds = false
-    isPanYIdle.get((idle) => {
-      if (idle) {
-        console.log("idle y")
-        lastBoyY = howFarOutOfBounds.y > 0 ? true : howFarOutOfBounds.y < 0 ? false : undefined
-        yWasAtIdelOutOfBounds = howFarOutOfBounds.y !== 0
-        yFadeBackIntoView.resume()
-      }
-      else yFadeBackIntoView.cancel()
-    }, false)
-
-
-
+    })
 
 
     const pz = new PanZ({
