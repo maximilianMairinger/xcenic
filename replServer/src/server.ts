de7b0739bc7c1bc711de047f9eeba1496d7e910a
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2022-11-14 01:12:32 +0100

    test change

--- replServer/src/server.ts
+++ replServer/src/server.ts
@@ -10,7 +10,7 @@ app.post("/addEntry", (req, res) => {
   const entry = req.body
   entry.time = Date.now()
 
-  console.log("Getting entry", entry)
+  console.log("getting entry", entry)
   
 
   setTimeout(() => {
